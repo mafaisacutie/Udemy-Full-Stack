d3983268d48f10a5e390d65c035981190a55c733
Mafa is a cutie <tasweeney1471@gmail.com>
2019-02-10 02:20:51 -0500

    DOM Part One

---
+++ DOM/Part_One/dom.js
@@ -0,0 +1,11 @@
+// Uses sorta
+
+// document.URL
+// document.body
+// document.head
+// document.links
+// document.getElementById()
+// document.getElementByClass()
+// document.getElementByTagName()
+// document.querySelector() // use any css selector
+// document.querySelectorAll() // returns list of all matches
