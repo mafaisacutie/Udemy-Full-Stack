d2ab069245d1dd08dc837c45339c29e43f48953f
Mafa is a cutie <tasweeney1471@gmail.com>
2019-02-11 18:19:06 -0500

    Python Level One Practice

---
+++ BACK_END/GAME_PRACTICE/game.py
@@ -0,0 +1,32 @@
+import random
+
+def get_guess():
+    return list(input("What is your guess? "))
+def generate_code():
+    digits = [str(num) for num in range(10)]
+    random.shuffle(digits)
+    return digits[:3]
+
+def generate_clues(code,user_guess):
+    if user_guess==code:
+        return "CODE CRACKED!"
+    clues = []
+    for ind,num in enumerate(user_guess):
+        if num==code[ind]:
+            clues.append("match")
+        elif num in code:
+            clues.append("close")
+        if clues == []:
+            return ["Nope"]
+        else:
+            return clues
+
+print("Welcome Code Breaker!")
+secretCode = generate_code()
+clueReport = []
+while clueReport!= "CODE CRACKED!":
+    guess = get_guess()
+    clueReport = generate_clues(guess,secretCode)
+    print("Here is the result of your guess: ")
+    for clue in clueReport:
+        print(clue)
