0201acdd515afe1d2126e12b90de3b8612e182d9
Mafa is a cutie <tasweeney1471@gmail.com>
2019-02-11 18:49:33 -0500

    Python Level Two Part Three

--- BACK_END/PYTHON_LEVEL_TWO/OOP.py
+++ BACK_END/PYTHON_LEVEL_TWO/OOP.py
@@ -4,3 +4,37 @@ class Sample():
 
 x = Sample()
 # print(type(x))
+
+# PART TWO
+class Dog():
+
+    # CLASS OBJECT ATTRIBUTE
+    species = "Mammal"
+
+    def __init__(self,breed,name):
+        self.breed = breed
+        self.name = name
+
+
+
+mydog = Dog("Lab","Sammy")
+# print(mydog.breed)
+# print(mydog.name)
+# print(mydog.species)
+
+class Circle():
+    pi = 3.14
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return self.radius*self.radius*Circle.pi
+
+    def set_radius(self,new_radius):
+        self.radius = new_radius
+
+
+myc = Circle(3)
+myc.set_radius(123)
+print(myc.area())
